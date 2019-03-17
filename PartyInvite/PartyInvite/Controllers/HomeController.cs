bd7fee6ef631796b4ee3c17fadc2948bd6579321
Rookaya Patel <rookaya.patel.yrd@gmail.com>
2019-03-17 16:39:41 -0400

    Finish Party Invite Web App

---
+++ PartyInvite/PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvite.Models;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                Respository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            } else
+            {
+                return View();
+            }
+            
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Respository.Respones.Where(r => r.WillAttend == true));
+        }
+    }
+}
